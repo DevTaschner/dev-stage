5a361772cc9b156fd5a01fb990d3c780cb737ea8
Guilherme da Silveira Taschner <dev.taschner@gmail.com>
2025-02-19 23:53:51 -0300

    refactor: moved main container
    Add main container into layout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next';
-import { Montserrat, Oxanium } from 'next/font/google';
-import './global.css';
+import type { Metadata } from "next";
+import { Montserrat, Oxanium } from "next/font/google";
+import "./global.css";
 
 export const metadata: Metadata = {
-  title: 'Dev Stage',
-  description: 'NLW project 2025',
+  title: "Dev Stage",
+  description: "NLW project 2025",
 };
 
 const oxanium = Oxanium({
-  weight: ['500', '600'],
-  subsets: ['latin'],
-  variable: '--font-oxanium',
+  weight: ["500", "600"],
+  subsets: ["latin"],
+  variable: "--font-oxanium",
 });
 
 const montserrat = Montserrat({
-  weight: ['400', '600'],
-  subsets: ['latin'],
-  variable: '--font-montserrat',
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
 });
 
-const font_variables = [oxanium.variable, montserrat.variable].join(' ');
+const font_variables = [oxanium.variable, montserrat.variable].join(" ");
 
 export default function RootLayout({
   children,
@@ -28,7 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={font_variables}>
-      <body>{children}</body>
+      <body className="bg-gray-900 bg-[url(/background.png)] bg-top bg-no-repeat text-gray-100 antialiased md:bg-right-top">
+        <main className="mx-auto max-w-[1240px] px-5 py-8 md:py-0">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
