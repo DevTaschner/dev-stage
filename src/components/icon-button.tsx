48661d47a6b0a64177222586d8810d36da883c6c
Guilherme da Silveira Taschner <dev.taschner@gmail.com>
2025-02-19 21:22:01 -0300

    style: Adjust style icon button
    adjust justify position children

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -7,7 +7,7 @@ export const IconButton = ({
   return (
     <button
       className={[
-        "text-blue hover:bg-blue flex size-8 w-full cursor-pointer items-center justify-between rounded-md bg-gray-500 px-5 transition-colors duration-300 hover:text-gray-900",
+        "text-blue hover:bg-blue flex size-8 cursor-pointer items-center justify-center rounded-md bg-gray-500 px-1.5 transition-colors duration-300 hover:text-gray-900",
         className,
       ].join(" ")}
       {...props}
